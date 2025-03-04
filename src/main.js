6880064825d5f21b32522e94799978ef3320f618
Taras <taras@coderiver.com.ua>
2025-03-04 10:21:03 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './scss/style.scss';
+
+const body = document.body;
+const burger = body.querySelector('.burger');
+const burgerLine = burger.querySelector('.burger__line');
+const sidebar = body.querySelector('.sidebar');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('burger--active');
+  burgerLine.classList.toggle('burger__line--active');
+  sidebar.classList.toggle('sidebar--active');
+})
